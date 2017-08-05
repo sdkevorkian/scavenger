9a463364c6a985732fe8aa44475b6d30dd066749
sdkevorkian <sdkevorkian@gmail.com>
2017-08-04 21:06:35 -0700

    reminder to use nodemon

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -4,11 +4,11 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json([{
-    "id": "1",
-    "username": "samsepi0l"
+    id: 1,
+    username: "samsepi0l"
   }, {
-    "id": "2",
-    "username": "D0loresH4ze"
+    id: 2,
+    username: "D0loresH4ze"
   }]);
 });
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,26 @@
 var React = require('react');
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            users: []
+        }
+    }
+
+componentDidMount() {
+    fetch('/users')
+        .then(res => res.json())
+        .then(users => this.setState({ users }));
+  }
+
     render() {
         return (
                     <div className="container">
                         <h1 className="title">Scavenger</h1>
+                        {this.state.users.map(user =>
+                            <div key={user.id}>{user.username}</div>
+                        )}
                     </div>
         );
     }
